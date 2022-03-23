c5ec131a0bb66b5026a306fd7305e9eaa75736f3
HoucemZribi <houcem.zribi@isimg.tn>
2022-03-23 13:52:01 +0100

    update credentials

--- src/main/java/com/example/rest/CRUD.java
+++ src/main/java/com/example/rest/CRUD.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 @Setter
 @Getter
 public class CRUD {
-    private String document_id;
+    private String documentId;
     private String name;
-    private String profession;
+
 }
